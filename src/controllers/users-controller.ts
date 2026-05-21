a532d27344caf266f1eda922893e98d9732865ac
Douglas <dodogm7@gmail.com>
2026-05-21 15:59:48 -0300

    creating project

---
+++ src/controllers/users-controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from "express";
+
+class UsersController {
+    create (request: Request, response: Response, next: NextFunction) {
+        return response.json({message: "ok"})
+    }
+}
+
+export {UsersController}
