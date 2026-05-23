29620d40ed8626796b2db434e359ca0d8123a065
Douglas <dodogm7@gmail.com>
2026-05-23 20:07:26 -0300

    Creating an authentication route and adding the prism.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 
 import { usersRouter } from "./users-routes";
+import { sessionRoutes } from "./sessions-routes";
 
 const routes = Router()
+
 routes.use("/users", usersRouter)
+routes.use("/sessions", sessionRoutes)
 
 export {routes}
